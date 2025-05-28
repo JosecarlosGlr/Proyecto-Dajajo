f1e72c1fa7f24f9814f3b08758e03a1031691194
jose carlos <josecar400@gmail.com>
2025-05-28 19:32:40 +0200

    #7 Comprobar calendario

--- discoteca.js
+++ discoteca.js
@@ -1,4 +1,9 @@
+document.getElementById("reservaForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const entradas = parseInt(document.getElementById("entradas").value);
 if (edad < 18) {
     mensaje.textContent = "Debes ser mayor de 18 años para comprar entradas.";
     return;
 }
+});
