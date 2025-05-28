22471f1105ac30600b0d57a3e125465a9074aeeb
jose carlos <josecar400@gmail.com>
2025-05-28 19:33:14 +0200

    #8 Comprobar inputs principales

--- discoteca.js
+++ discoteca.js
@@ -1,6 +1,10 @@
 document.getElementById("reservaForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellidos = document.getElementById("apellidos").value.trim();
+    const edad = parseInt(document.getElementById("edad").value);
+    const dni = document.getElementById("dni").value.trim();
     const entradas = parseInt(document.getElementById("entradas").value);
 if (edad < 18) {
     mensaje.textContent = "Debes ser mayor de 18 años para comprar entradas.";
