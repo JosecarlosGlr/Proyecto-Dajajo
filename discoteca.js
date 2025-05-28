4c007078e0226a2df76a4e8b25620f03239a2f47
jose carlos <josecar400@gmail.com>
2025-05-28 19:34:43 +0200

    #9 Validar tarjeta

--- discoteca.js
+++ discoteca.js
@@ -6,8 +6,23 @@ document.getElementById("reservaForm").addEventListener("submit", function(e) {
     const edad = parseInt(document.getElementById("edad").value);
     const dni = document.getElementById("dni").value.trim();
     const entradas = parseInt(document.getElementById("entradas").value);
-if (edad < 18) {
-    mensaje.textContent = "Debes ser mayor de 18 años para comprar entradas.";
-    return;
-}
-});
+
+    const mensaje = document.getElementById("mensaje");
+    mensaje.style.color = "red";
+
+    // Validar campos vacíos
+    if (!nombre || !apellidos || !edad || !dni || !entradas || !fechaEvento || !numeroTarjeta || !fechaCaducidad) {
+        mensaje.textContent = "Por favor, completa todos los campos.";
+        return;
+    }
+
+    if (edad < 18) {
+        mensaje.textContent = "Debes ser mayor de 18 años para comprar entradas.";
+        return;
+    }
+    // Validar número de tarjeta (solo números, 16 dígitos)
+    if (!/^\d{16}$/.test(numeroTarjeta)) {
+        mensaje.textContent = "Número de tarjeta inválido (debe contener 16 dígitos).";
+        return;
+        }
+    });
